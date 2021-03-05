ced11d6dc837726f169a7d4481fd3b38d673f3cf
Wladimir Rocha <wld.rocha@gmail.com>
2021-03-04 22:23:54 -0400

    excercise 6

--- index.js
+++ index.js
@@ -84,3 +84,25 @@ function doubleOddNumbers(array) {
 
 console.log(doubleOddNumbers([1, 2, 3, 4, 5]));
 
+/*
+Exercise 6
+Write a function called extractKey which accepts an array of objects and some key and returns a new array with the value of that key in each object.
+
+*/
+
+function extractKey(array, key) {
+  return array.map((el) => {
+    if (el.hasOwnProperty(key)) return el[key];
+  });
+}
+
+// extractKey(
+//   [{ name: "Elie" }, { name: "Tim" }, { name: "Matt" }, { name: "Colt" }],
+//   "name"
+// );
+console.log(
+  extractKey(
+    [{ name: "Elie" }, { name: "Tim" }, { name: "Matt" }, { name: "Colt" }],
+    "name"
+  )
+);
