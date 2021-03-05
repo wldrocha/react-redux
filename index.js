7419a276d45714a06ba675bea89c5485cfab5549
Wladimir Rocha <wld.rocha@gmail.com>
2021-03-04 22:07:31 -0400

    excercise 5

--- index.js
+++ index.js
@@ -70,3 +70,17 @@ function removeVowels(string) {
 console.log(removeVowels("Wladimir"));
 
 
+/*
+Exercise 5
+Write a function called doubleOddNumbers which accepts an array and returns a new array with all of the odd numbers doubled (HINT - you can use map and fitler to double and then filter the odd numbers).
+
+*/
+
+function doubleOddNumbers(array) {
+  const odd = array.filter((elm) => elm % 2);
+  const double = odd.map((el) => el * 2);
+  return double;
+}
+
+console.log(doubleOddNumbers([1, 2, 3, 4, 5]));
+
