cbbc3ceb2a302f5184ad068f082fca7d82d0692f
Wladimir Rocha <wldrocha@gmail.com>
2023-09-20 15:04:09 -0400

    [ADD] link to home on icon header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 import '../assets/styles/components/Header.scss'
 import logo from '../assets/static/logo.png'
 import burguer_menu from '../assets/static/burguer_menu.png'
+import { MdMenu } from 'react-icons/md'
 
 export const Header = () => {
   function closeMenu() {
@@ -10,22 +11,24 @@ export const Header = () => {
   }
 
   return (
-    <header className='header'>
-      <div className='container__images'>
-        <figure className='header__logo'>
-          <img src={logo} alt='' className='header__img' />
+    <header className="header">
+      <div className="container__images">
+        <figure className="header__logo">
+          <Link to="/" className="option__items" onClick={closeMenu}>
+            <img src={logo} alt="" className="header__img" />
+          </Link>
         </figure>
-        <label className='nav__burguer' id='labelCheck' htmlFor='check'>
-          <img src={burguer_menu} alt='' />
+        <label className="nav__burguer" id="labelCheck" htmlFor="check">
+          <MdMenu />
         </label>
       </div>
-      <input type='checkbox' name='check' id='check' />
-      <div className='container__options' id='containerOptions'>
-        <nav className='nav__options'>
-          <Link to='/' className='option__items' onClick={closeMenu}>
+      <input type="checkbox" name="check" id="check" />
+      <div className="container__options" id="containerOptions">
+        <nav className="nav__options">
+          <Link to="/" className="option__items" onClick={closeMenu}>
             Home
           </Link>
-          <Link to='/users' className='option__items' onClick={closeMenu}>
+          <Link to="/users" className="option__items" onClick={closeMenu}>
             Usuarios
           </Link>
         </nav>
